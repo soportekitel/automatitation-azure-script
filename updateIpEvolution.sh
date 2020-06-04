8c095cdd965d50ef8e83e570898db1d7a7fed7c8
Jose Rojas <jose.rojas@despegar.com>
2020-06-04 00:57:19 -0300

    Fix updateIpEvolution

--- updateIpEvolution.sh
+++ updateIpEvolution.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-EVOLUTION=$(jq -r .evolution config.json )
+EVOLUTION=$(jq -r .evolution /usr/local/infodes/bin/config.json )
 sed -Ei "s/[0-9\.]+.EvoRouter/$EVOLUTION\/EvoRouter/" /etc/asterisk/extensions_custom.conf
-asterisk -rx "dialplan reload" 
+asterisk -rx "dialplan reload"
