9ae69dbdaf335ffa0e798c887a89255dceb54659
Jose Rojas <jose.rojas@despegar.com>
2020-07-09 09:24:49 -0300

    Fix backup_table_cdr.sh

--- backup_table_cdr.sh
+++ backup_table_cdr.sh
@@ -3,18 +3,9 @@
 backup_dir="/var/spool/asterisk/cdr_backup/"$(date -d "-1 months" +%Y)
 backup_file="CDR_"$(date -d "-1 months" +%Y)"_"$(date -d "-1 months" +%m)".csv"
 
-function check_file() {
-    [[ -n "$1" ]] && {
-        [[ -f "$1" ]] && rm -f "$1"
-    }
-}
-
 [ ! -d $backup_dir ] && mkdir -p $backup_dir && chown asterisk: $backup_dir && chmod 777 $backup_dir
 
-check_file /tmp/$backup_file"_header" 
-check_file $backup_dir"/"$backup_file"_header"
-check_file /tmp/$backup_file"_data"
-check_file $backup_dir"/"$backup_file"_data"
+rm -f /tmp/$backup_file[data,header]
 
 mysql -u admin -e "select GROUP_CONCAT(CONCAT(\"\",COLUMN_NAME,\"\")) as col_names from INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'cdr' AND TABLE_SCHEMA ='asteriskcdrdb' ORDER BY ORDINAL_POSITION" | grep -v col_names | sed 's/,/","/g' | sed -E 's/^|$/"/g' > /tmp/$backup_file"_header"
 
